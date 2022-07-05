3b9d7b8805c178dfb447cc94bf23f635f77037ac
Juan Serrano <juansdev72@gmail.com>
2022-07-04 20:01:19 -0500

    Arreglar commit

--- assets/js/text_diagonal.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-    var arr = $('.holder').text().split(' ');
-    var result = '<span class="rotate"> ' +
-        arr.join(' </span><span class="rotate">') +
-        '</span>';
-    // $('.holder').html(result);
-});
