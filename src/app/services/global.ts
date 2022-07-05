8ce56c07137c71c2ff2a53cad8f709fd5ed4d783
Juan Serrano <juansdev72@gmail.com>
2022-07-04 20:21:11 -0500

    Actualice http a https en los endpoints

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,9 +1,4 @@
 export const global = {
-<<<<<<< HEAD
     url_frontend: '/',
     url_backend_secop: 'https://ds4a-secop.azurewebsites.net/api/secop/'
-=======
-    url_frontend: 'http://localhost:4200/',
-    url_backend_secop: '/api/secop/'
->>>>>>> 42c1c93db10fc7601cdd7a7af0be97298c46e8b3
 };
